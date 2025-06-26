e60dd643449ef40897b928a6a539c52697f0ae28
Hamza Farrukh <hamzafarrukh997@gmail.com>
2025-06-26 18:20:34 +0500

    Update project files

--- backend/server.js
+++ backend/server.js
@@ -42,5 +42,10 @@ app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
